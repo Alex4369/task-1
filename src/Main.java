9fdd7b3a743c6865131d376c34e8f3a1f26136e2
Aleksei Mamontov <amamont868617@gmail.com>
2021-10-21 20:32:09 +0300

    task

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 5000;
+        int miles = 20;
+        System.out.println("Начисленные мили");
+        int bonusMiles = priceTicket / miles;
+        System.out.println(bonusMiles);
+
+    }
+}
